ea6b4b15f9d629af6255001e0f7e54a71423c549
youxin <annidy@gmail.com>
2015-03-30 17:24:12 +0800

    Initial Commit

---
+++ tm-client/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  tm-client
+//
+//  Created by FengXing on 3/30/15.
+//  Copyright (c) 2015 annidy. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
